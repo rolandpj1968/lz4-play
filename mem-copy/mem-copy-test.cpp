df8940e2bfc94092df3df25dcb14e44268b25fcf
rolandp <rolandp@amazon.com>
2021-07-03 13:50:28 +0200

    Full misaligned mem-copy coverage and fiddles

--- mem-copy/mem-copy-test.cpp
+++ mem-copy/mem-copy-test.cpp
@@ -8,10 +8,14 @@
 typedef std::chrono::high_resolution_clock Time;
 typedef std::chrono::duration<double> dsec;
 
-static u8 src[1 << 21 + 7];
-static u8 dst[1 << 21 + 7];
+const size_t MiB = 1 << 20;
 
-static const size_t copy_len = 1 << 21;
+const double ms_per_s = 1000.0;
+
+static u8 src[2*MiB + 7];
+static u8 dst[2*MiB + 7];
+
+static const size_t copy_len = 2*MiB;
 
 static double time_mem_copy_ms(void* dst, const void* src, size_t len, unsigned n_iters) {
   auto t0 = Time::now();
@@ -24,7 +28,7 @@ static double time_mem_copy_ms(void* dst, const void* src, size_t len, unsigned 
   dsec ds1 = t1 - t0;
   double secs1 = ds1.count();
 
-  return secs1 * 1000.0/*ms per s*/;
+  return secs1 * ms_per_s;
 }
 
 static double time_memcpy_ms(void* dst, const void* src, size_t len, unsigned n_iters) {
@@ -38,7 +42,7 @@ static double time_memcpy_ms(void* dst, const void* src, size_t len, unsigned n_
   dsec ds1 = t1 - t0;
   double secs1 = ds1.count();
 
-  return secs1 * 1000.0/*ms per s*/;
+  return secs1 * ms_per_s;
 }
 
 int main(int argc, char* argv[]) {
@@ -52,19 +56,20 @@ int main(int argc, char* argv[]) {
   for(unsigned i = 0; i < n_loops; i++ ) {
   
     for(size_t src_offset = 0; src_offset < 8; src_offset++) {
-      double ms;
-
-      ms = time_mem_copy_ms(dst, src + src_offset, copy_len, n_iters);
-      printf("mem_copy: copied %zu bytes to %p from %p repeated %u times in %7.3lfms\n", copy_len, dst, src + src_offset, n_iters, ms);
-      ms = time_memcpy_ms(dst, src + src_offset, copy_len, n_iters);
-      printf("memcpy:   copied %zu bytes to %p from %p repeated %u times in %7.3lfms\n", copy_len, dst, src + src_offset, n_iters, ms);
-    }
-    
-    printf("\n");
-    
-    for(size_t dst_offset = 0; dst_offset < 8; dst_offset++) {
-      mem_copy_u64(dst + dst_offset, src, copy_len);
-      printf("Copied %zu bytes to %p from %p\n", copy_len, dst + dst_offset, src);
+      for(size_t dst_offset = 0; dst_offset < 8; dst_offset++) {
+	double ms;
+	double mib_per_s;
+	
+	ms = time_mem_copy_ms(dst + dst_offset, src + src_offset, copy_len, n_iters);
+	
+	mib_per_s = copy_len*n_iters/MiB / (ms/ms_per_s);
+	printf("mem_copy: copied %zu bytes to %p from %p repeated %u times in %7.3lfms - %10.3lfMiB/s\n", copy_len, dst + dst_offset, src + src_offset, n_iters, ms, mib_per_s);
+	
+	ms = time_memcpy_ms(dst + dst_offset, src + src_offset, copy_len, n_iters);
+	
+	mib_per_s = copy_len*n_iters/MiB / (ms/ms_per_s);
+	printf("memcpy:   copied %zu bytes to %p from %p repeated %u times in %7.3lfms - %10.3lfMiB/s\n\n", copy_len, dst + dst_offset, src + src_offset, n_iters, ms, mib_per_s);
+      }
     }
     
     printf("\n");
