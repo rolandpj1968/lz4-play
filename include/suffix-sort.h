515317c4d121ed728412221577e610e3b638dc84
rolandp <rolandp@amazon.com>
2021-08-09 09:20:47 +0200

    Simple (brute-force) LCP

--- include/suffix-sort.h
+++ include/suffix-sort.h
@@ -4,6 +4,7 @@
 #include <types.h>
 
 #define SUFFIX_SORT_OK 0
+#define LCP_OK 0
 
 #ifdef __cplusplus
 extern "C" {
@@ -11,10 +12,25 @@ extern "C" {
 
 /**
  * Populate the suffix array SA with the suffix sort of data.
+ * Naive N^2logN algorithm.
  * @return rc
  */
 extern int simple_suffix_sort(const u8* data, const u32 len, u32* SA);
-  
+
+/**
+ * Populate the longest common prefix array LCP from the index array SA.
+ * Naive N^2 algorithm.
+ * @return rc
+ */
+extern int simple_lcp(const u8* data, const u32 len, const u32* SA, u32* LCP);
+
+/**
+ * Populate the suffix array SA with the suffix sort of data.
+ * Naive N^2logN + N^2 algorithms.
+ * @return rc
+ */
+extern int simple_suffix_sort_with_lcp(const u8* data, const u32 len, u32* SA, u32* LCP);
+
 #ifdef __cplusplus
 }
 #endif
--- suffix-sort/simple/suffix-sort.cpp
+++ suffix-sort/simple/suffix-sort.cpp
@@ -5,7 +5,6 @@
 #include <suffix-sort.h>
 
 namespace SimpleSuffixSort {
-  
 
   struct SuffixLess {
     const u8* data;
@@ -31,12 +30,24 @@ namespace SimpleSuffixSort {
     }
   }; // struct SuffixLess
 
+  u32 common_prefix_len(const u8* data, const u32 len, const u32 i1, const u32 i2) {
+      const u8* s1 = data + i1;
+      const u32 len1 = len - i1;
+      const u8* s2 = data + i2;
+      const u32 len2 = len + i2;
+
+      const u32 min_len = std::min(len1, len2);
+
+      u32 prefix_len = 0;
+      while(prefix_len < min_len && s1[prefix_len] == s2[prefix_len]) {
+	prefix_len++;
+      }
+
+      return prefix_len;
+  }
+
 } // namespace SimpleSuffixSort
 
-/**
- * Populate the suffix array SA with the suffix sort of data.
- * @return rc
- */
 int simple_suffix_sort(const u8* data, const u32 len, u32* SA) {
   // Initialise the suffix array SA with normal order
   for(u32 i = 0; i < len; i++) {
@@ -48,7 +59,33 @@ int simple_suffix_sort(const u8* data, const u32 len, u32* SA) {
 
   return SUFFIX_SORT_OK;
 }
-  
+
+int simple_lcp(const u8* data, const u32 len, const u32* SA, u32* LCP) {
+  if(len == 0) {
+    return LCP_OK;
+  }
+
+  LCP[0] = 0;
+  for(u32 index = 1; index < len; index++) {
+    LCP[index] = SimpleSuffixSort::common_prefix_len(data, len, SA[index-1], SA[index]);
+  }
+
+  return LCP_OK;
+}
+
+int simple_suffix_sort_with_lcp(const u8* data, const u32 len, u32* SA, u32* LCP) {
+  int rc;
+
+  rc = simple_suffix_sort(data, len, SA);
+  if(rc != SUFFIX_SORT_OK) {
+    return rc;
+  }
+
+  rc = simple_lcp(data, len, SA, LCP);
+
+  return rc;
+}
+
 int main() {
   printf("Hallo RPJ\n");
   
@@ -56,11 +93,12 @@ int main() {
   const u32 len = strlen(data);
 
   u32* SA = new u32[len];
+  u32* LCP = new u32[len];
 
-  int rc = simple_suffix_sort((const u8*)data, len, SA);
+  int rc = simple_suffix_sort_with_lcp((const u8*)data, len, SA, LCP);
 
   if(rc) {
-    fprintf(stderr, "simple_suffix_sort failed with rc %d\n", rc);
+    fprintf(stderr, "simple_suffix_sort_with_lcp failed with rc %d\n", rc);
     exit(1);
   }
 
@@ -69,6 +107,6 @@ int main() {
   for(u32 i = 0; i < len; i++) {
     u32 index = SA[i];
     
-    printf("%2d: %.*s\n", i, len-index, data+index);
+    printf("%8d [CP %8d]: %.*s\n", i, LCP[i], len-index, data+index);
   }
 }
